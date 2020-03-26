9d94b58deb2e3db5ee67c84a5e21ff26b60b6192
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-26 16:55:45 -0300

    Add new controllers folder and struct to the project

---
+++ src/app/controladores/livro-contolador.js
@@ -0,0 +1,37 @@
+const { validationResult } = require('express-validator/check');
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
+
+class LivroControlador {
+  lista() {
+    return function(req, resp) {
+
+      const livroDao = new LivroDao(db);
+      livroDao.lista()
+              .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                  livros: livros
+                }
+              ))
+              .catch(erro => console.log(erro));
+    };
+  }
+
+  formularioCadastro() {
+    return function(req, resp) {
+      resp.marko(
+          require('../views/livros/form/form.marko'),
+          { livro: {} });
+  }
+  }
+
+
+
+
+
+
+}
+
+module.exports = LivroControlador;
