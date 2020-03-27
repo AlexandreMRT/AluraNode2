15a9aab1c08b637c301107ef3f6ce5283eb3b510
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-03-26 21:45:16 -0300

    Update routes to use specific folders

---
+++ src/app/rotas/livro-rotas.js
@@ -0,0 +1,23 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
+const LivroControlador = require('../controladores/livro-contolador');
+const livroControlador = new LivroControlador();
+
+const Livro = require('../modelos/livro')
+
+module.exports = (app) => {
+
+  const rotasLivro = LivroControlador.rotas();
+
+  app.get(rotasLivro.lista, livroControlador.lista());
+
+  app.route(rotasLivro.cadastro)
+      .get(livroControlador.formularioCadastro())
+      .post(Livro.validacoes(), livroControlador.cadastra())
+      .put(livroControlador.edita());
+
+  app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
+
+  app.delete(rotasLivro.delecao, livroControlador.remove());
+};
